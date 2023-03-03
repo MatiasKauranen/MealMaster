f0feef89b4420ad95f3ee4923f37aa2b406bf440
MatiasKauranen <94970023+MatiasKauranen@users.noreply.github.com>
2023-03-04 00:32:59 +0200

    Send recipes via email
    Now you can send favorite recipes via email.

--- lib/favorites_screen.dart
+++ lib/favorites_screen.dart
@@ -6,6 +6,7 @@ import 'recipe_screen.dart';
 import 'package:mealmaster/components/navbar.dart';
 import 'home_screen.dart';
 import 'package:mealmaster/settings_screen.dart';
+import 'package:flutter_email_sender/flutter_email_sender.dart';
 
 class FavoritesScreen extends StatefulWidget {
   static const String id = 'favorites_screen';
@@ -16,6 +17,8 @@ class FavoritesScreen extends StatefulWidget {
 
 class _FavoritesScreenState extends State<FavoritesScreen> {
   late List<String> _favoriteRecipes;
+  final _recipientController = TextEditingController();
+
   @override
   void initState() {
     super.initState();
@@ -59,13 +62,107 @@ class _FavoritesScreenState extends State<FavoritesScreen> {
     }
   }
 
-  Future<dynamic> _getRecipe(String recipeName) async {
-    final url =
-        'https://www.themealdb.com/api/json/v1/1/search.php?s=${recipeName.replaceAll(' ', '%20')}';
-    final response = await http.get(Uri.parse(url));
-    final data = jsonDecode(response.body);
-    return data['meals'][0];
+  Future<Map<String, dynamic>> _getRecipe(String recipeName) async {
+  final url =
+      'https://www.themealdb.com/api/json/v1/1/search.php?s=${recipeName.replaceAll(' ', '%20')}';
+  final response = await http.get(Uri.parse(url));
+  final data = jsonDecode(response.body);
+  final recipe = data['meals'][0];
+  final imageUrl = recipe['strMealThumb'];
+  final ingredients = [    recipe['strIngredient1'],
+    recipe['strIngredient2'],
+    recipe['strIngredient3'],
+    recipe['strIngredient4'],
+    recipe['strIngredient5'],
+    recipe['strIngredient6'],
+    recipe['strIngredient7'],
+    recipe['strIngredient8'],
+    recipe['strIngredient9'],
+    recipe['strIngredient10'],
+    recipe['strIngredient11'],
+    recipe['strIngredient12'],
+    recipe['strIngredient13'],
+    recipe['strIngredient14'],
+    recipe['strIngredient15'],
+    recipe['strIngredient16'],
+    recipe['strIngredient17'],
+    recipe['strIngredient18'],
+    recipe['strIngredient19'],
+    recipe['strIngredient20'],
+  ].where((ingredient) => ingredient != null && ingredient.isNotEmpty).toList();
+  final instructions = recipe['strInstructions'];
+
+  return {
+    'imageUrl': imageUrl,
+    'ingredients': ingredients,
+    'instructions': instructions,
+  };
+}
+
+Future<void> _sendRecipeByEmail(String recipeName) async {
+  final subject = 'Recipe: $recipeName';
+  final recipe = await _getRecipe(recipeName);
+  final imageUrl = recipe['imageUrl'];
+  final ingredients = recipe['ingredients'];
+  final instructions = recipe['instructions'];
+  final isHTML = true;
+
+  // Get the meal ID to create the meal page link
+  final mealId = recipe['idMeal'];
+  final mealPageLink = 'https://www.themealdb.com/meal/$mealId';
+
+  final recipient = await showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('Send Recipe'),
+        content: TextField(
+          controller: _recipientController,
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+            labelText: 'Recipient',
+          ),
+        ),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(null),
+            child: Text('CANCEL'),
+          ),
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(_recipientController.text),
+            child: Text('OK'),
+          ),
+        ],
+      );
+    },
+  );
+  if (recipient != null) {
+    final imageBytes = await http.readBytes(Uri.parse(imageUrl));
+    final base64Image = base64Encode(imageBytes);
+    final imageHtml = '<img src="data:image/png;base64,$base64Image">';
+    final email = Email(
+      body: '''
+        $imageHtml
+        <h2>Ingredients</h2>
+        <ul>
+          ${ingredients.map((ingredient) => '<li>$ingredient</li>').join('\n')}
+        </ul>
+        <h2>Instructions</h2>
+        <p>$instructions</p>
+        <p><a href="$mealPageLink">View on MealDB</a></p>
+      ''',
+      subject: subject,
+      recipients: [recipient],
+      isHTML: isHTML,
+    );
+    await FlutterEmailSender.send(email);
   }
+}
+
+
+
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -96,11 +193,22 @@ class _FavoritesScreenState extends State<FavoritesScreen> {
                         final recipeName = _favoriteRecipes[index];
                         return ListTile(
                           title: Text(recipeName),
-                          trailing: IconButton(
-                            icon: Icon(Icons.delete),
-                            onPressed: () {
-                              _removeFavoriteRecipe(index);
-                            },
+                          trailing: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              IconButton(
+                                icon: Icon(Icons.delete),
+                                onPressed: () {
+                                  _removeFavoriteRecipe(index);
+                                },
+                              ),
+                              IconButton(
+                                icon: Icon(Icons.email),
+                                onPressed: () {
+                                  _sendRecipeByEmail(recipeName);
+                                },
+                              ),
+                            ],
                           ),
                           onTap: () async {
                             final recipe = await _getRecipe(recipeName);
