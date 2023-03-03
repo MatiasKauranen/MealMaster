5c4df81bc58ee03856aaf782110d247d6d7c866c
MatiasKauranen <94970023+MatiasKauranen@users.noreply.github.com>
2023-03-04 00:56:59 +0200

    Images

--- lib/settings_screen.dart
+++ lib/settings_screen.dart
@@ -15,9 +15,6 @@ class _SettingsScreenState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Settings'),
-      ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
